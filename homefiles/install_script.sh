727ca226bcdde326556364378595636f8dca8650
BToast7274 <156036299+BToast7274@users.noreply.github.com>
2025-03-16 15:14:21 -0400

    Finished install_script.sh probably maybe

--- homefiles/install_script.sh
+++ homefiles/install_script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 git clone https://github.com/BToast7274/dotfiles/blob/main/homefiles/.bashrc
+mv .bashrc .bashrc_backup
+cd /BToast7274/dotfiles/blob/main/homefiles/
+mv .bashrc ~/
+echo "Remember to reload your enviornment with [source ~/.bashrc]"
+
+
